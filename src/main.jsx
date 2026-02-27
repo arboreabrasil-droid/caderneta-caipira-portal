2a6287815b27f215372fc323b90395bed837c04d
arboreabrasil-droid <arboreabrasil@gmail.com>
2026-02-27 22:46:02 +0000

    feat: aceite de termos funcionando com log no Firestore

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
 import Convite from './Convite'
-import Termos from './Termos'   
+import Termos from './Termos'          // ← ADICIONE ESTA LINHA
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
