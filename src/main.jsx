1421ec1b3f0dd3c270b15676b7de50b496d87d4c
arboreabrasil-droid <arboreabrasil@gmail.com>
2026-02-27 22:15:23 +0000

    fix: importa componente Termos na rota

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,21 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
 import Convite from './Convite'
+import Termos from './Termos'   
 import './styles/index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/convite" element={<Convite />} />
+        <Route path="/termos" element={<Termos />} />   // ← GARANTA QUE ESTA ROTA EXISTE
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+)
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
